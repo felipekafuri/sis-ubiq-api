0ae76e562d6f9e2db793a2ef560e51d9939b4e4c
Felipe Kafuri <felipe11.rk@gmail.com>
2023-01-31 21:43:45 -0300

    initial commit

---
+++ app/views.py
@@ -0,0 +1,38 @@
+from datetime import timedelta
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from main import ACCESS_TOKEN_EXPIRE_MINUTES, Token, User, authenticate_user, create_access_token, get_current_active_user
+from main import app
+from fake_db.user import fake_users_db
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    data = {"sub": user.username, "scopes": ['room', 'bathroom']}
+    access_token = create_access_token(
+        data, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Test"}
